402557c5288f9e088fed182100c62ce513822359
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-03 11:26:01 +0530

    Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,6 @@ function App() {
   return (
     <div className="App">
       <h1>Codeial</h1>
-      <p>Codeial is a tool for creating and managing code reviews.</p>
-      <p>Codeial is a tool for creating and managing code reviews.</p>
-      <p>sdsdsdsdsdsdsdsd</p>
-      <h4>sddsdsdsdsdsdsds</h4>
     </div>
   );
 }
