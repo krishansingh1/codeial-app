1e6bfeb816ff6655d88a2d741bcb429f3fb6f291
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-04 19:13:03 +0530

    Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import getPosts from '../api';
 import Home from '../pages/Home';
+import Loader from './Loader';
+
 function App() {
+  const [post, setPost] = useState('');
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
       console.log('response', response);
+      setPost(response.data.posts);
     };
-
+    
     fetchPosts();
   }, []);
   return (
