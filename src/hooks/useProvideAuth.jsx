0578b3816ab36f8703997e46f9dfc2d66dcf1205
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-09 21:40:07 +0530

    Commit

--- src/hooks/useProvideAuth.jsx
+++ src/hooks/useProvideAuth.jsx
@@ -1,2 +1,16 @@
-export const useProvideAuth = () => {};
-    
+import { useState } from 'react';
+
+export const useProvideAuth = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const login = (email, password) => {};
+  const logout = () => {};
+
+  return {
+    user,
+    login,
+    logout,
+    loading,
+  };
+};
