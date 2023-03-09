e4256610f7c49741d4ad54893c77dac2a25eb81c
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-09 17:36:14 +0530

    Commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import styles from '../styles/login.module.css';
-import { useToasts } from 'react-toast-notifications';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loggingIn, setLogginIn] = useState(false);
-  const { addToast } = useToasts();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,40 +13,42 @@ const Login = () => {
     setLogginIn(true);
 
     if (!email || !password) {
-      return addToast('Please enter both email and password', {
-        appearance: 'error',
-      });
+      toast.error('Please enter both email and password');
+      return;
     }
   };
 
   return (
-    <form className={styles.loginForm} onSubmit={handleSubmit}>
-      <span className={styles.loginSignupHeader}>Log In</span>
+    <>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <span className={styles.loginSignupHeader}>Log In</span>
 
-      <div className={styles.field}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
+        <div className={styles.field}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-      <div className={styles.field}>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+        <div className={styles.field}>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <div className={styles.field}>
-        <button disabled={loggingIn}>
-          {loggingIn ? 'Logging In...' : 'Log In'}
-        </button>
-      </div>
-    </form>
+        <div className={styles.field}>
+          <button disabled={loggingIn}>
+            {loggingIn ? 'Logging In...' : 'Log In'}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
