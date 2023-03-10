d1c848e6af27661a9864a00c0a3f00acd0e27b27
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-10 22:35:37 +0530

    Commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,14 @@
 export * from './constants';
+
+export const getFormBody = (params) => {
+    let formBody = [];
+
+    for (let property in params) {
+        let encodedKey = encodeURIComponent(property);
+        let encodedValue = encodeURIComponent(params[property]);
+
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+
+    return formBody.join("&");
+}
