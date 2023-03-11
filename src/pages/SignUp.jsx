8a6b36b61f4f8a999a269b40a6fc38e030717311
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 17:30:14 +0530

    Commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,14 +4,21 @@ const SignUp = () => {
   return (
     <form className={styles.loginForm}>
       <span className={styles.loginSignupHeader}>Sign Up</span>
-      <div>
+      <div className={styles.field}>
         <input type="text" placeholder="First Name" />
       </div>
-      <div>
+      <div className={styles.field}>
         <input type="text" name="" id="" placeholder="Last Name" />
       </div>
-      <input type="email" name="email" placeholder="Email" />
-      <input type="password" name="" id="" placeholder="Password" />
+      <div className={styles.field}>
+        <input type="email" name="email" placeholder="Email" />
+      </div>
+      <div className={styles.field}>
+        <input type="password" name="" id="" placeholder="Password" />
+      </div>
+      <div className={styles.field}>
+        <button>Sign Up</button>
+      </div>
     </form>
   );
 };
