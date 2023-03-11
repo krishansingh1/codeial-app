be4db60de30182f79bdcf552018fe20bbcc30ff1
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 15:36:12 +0530

    Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,26 +5,28 @@ import Home from '../pages/Home';
 import Loader from './Loader';
 import Navbar from './Navbar';
 import Login from '../pages/Login';
+import { useAuth } from '../hooks/useProvideAuth';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [posts, setPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  const auth = useAuth();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await getPosts();
-      console.log('response', response);
-      if (response.success) {
-        setPosts(response.data.posts);
-      }
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const response = await getPosts();
+  //     console.log('response', response);
+  //     if (response.success) {
+  //       setPosts(response.data.posts);
+  //     }
 
-      setLoading(false);
-    };
+  //     setLoading(false);
+  //   };
 
-    fetchPosts();
-  }, []);
+  //   fetchPosts();
+  // }, []);
 
-  if (loading) {
+  if (auth.loading) {
     return <Loader />;
   }
 
@@ -33,7 +35,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home posts={posts} />} />
+          <Route path="/" element={<Home posts={[]} />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
