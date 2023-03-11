acb33862dae692f3c3400b4b5458aaf8bfabcbaa
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 19:24:40 +0530

    Commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -41,7 +41,7 @@ const SignUp = () => {
 
       return toast.success('User Signup success fully, please login now');
     } else {
-      return toast.error(response.message);
+      toast.error(response.message);
     }
 
     setSigningUp(false);
