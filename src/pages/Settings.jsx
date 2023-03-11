5fd9d929fc9a2d18298f8d396e7e0d82e868bbce
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 23:09:12 +0530

    Commit

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,10 +2,35 @@ import styles from '../styles/settings.module.css';
 
 const Settings = () => {
   return (
-    <div className={style.settings}>
+    <div className={styles.settings}>
       <div className={styles.imgContainer}>
         <img src="" alt="" />
+      </div>
+      <div className={styles.field}>
+        <div className={styles.fieldLabel}>Email</div>
+        <div className={styles.fieldValue}>{auth.user?.email}</div>
+      </div>
+
+      <div className={styles.field}>
+        <div className={styles.fieldLabel}>Name</div>
+        <div className={styles.fieldValue}>{auth.user?.name}</div>
+      </div>
+
+      <div className={styles.field}>
+        <div className={styles.fieldLabel}>Password</div>
+        <input type="password" />
+      </div>
+
+      <div className={styles.field}>
+        <div className={styles.fieldLabel}>Confirm Password</div>
+        <input type="password" />
+      </div>
+
+      <div className={styles.btnGrp}>
+        <button className={`button ${styles.editBtn}`}>Edit Profile</button>
       </div>
     </div>
   );
 };
+
+export default Settings;
