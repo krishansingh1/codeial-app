318339a3f4b4038a1d32359d5fbdfbe76bb1cfa1
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 15:54:54 +0530

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import styles from '../styles/navbar.module.css';
 import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useProvideAuth';
+
 const Navbar = () => {
+  const auth = useAuth();
   return (
     <div className={styles.nav}>
       <div className={styles.leftDiv}>
