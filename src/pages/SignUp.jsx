1a220ac86b6548da12f0e97cdc45b48ba352b430
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 18:07:51 +0530

    Commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,30 +1,65 @@
 import { useState } from 'react';
 import styles from '../styles/login.module.css';
+import toast, { Toaster } from 'react-hot-toast';
+import { useAuth } from '../hooks/useProvideAuth';
 
 const SignUp = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmpassword, setConfirmpassword] = useState('');
+  const auth = useAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password || !confirmpassword) {
+      toast.error('Please enter your responses in all the field');
+      return;
+    }
+  };
   return (
-    <form className={styles.loginForm}>
-      <span className={styles.loginSignupHeader}>Sign Up</span>
-      <div className={styles.field}>
-        <input type="text" placeholder="Name" />
-      </div>
-      <div className={styles.field}>
-        <input type="email" placeholder="Email" />
-      </div>
-      <div className={styles.field}>
-        <input type="password" placeholder="Password" />
-      </div>
-      <div className={styles.field}>
-        <input type="password" placeholder="Confirm Password" />
-      </div>
-      <div className={styles.field}>
-        <button>Sign Up</button>
-      </div>
-    </form>
+    <>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <span className={styles.loginSignupHeader}>Sign Up</span>
+        <div className={styles.field}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles.field}>
+          <input
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.field}>
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className={styles.field}>
+          <input
+            type="password"
+            value={confirmpassword}
+            placeholder="Confirm Password"
+            onChange={(e) => setConfirmpassword(e.target.value)}
+          />
+        </div>
+        <div className={styles.field}>
+          <button>Sign Up</button>
+        </div>
+      </form>
+    </>
   );
 };
 
