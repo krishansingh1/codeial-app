e8a8953d3de81feeb57de5556a247b4bce8fa5e3
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 10:32:29 +0530

    Commit

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,30 @@ export const setItemInLocalStorage = (value, key) => {
     if (!key || !value) {
         return console.error("Can not store in LS");
     }
+
+    const valueToStore = typeof value !== "string" ? JSON.stringify(value) : value;
+
+    localStorage.setItem(key, valueToStore);
+}
+
+export const getItemFromLocalStorage = (key) => {
+    if (!key) {
+        return console.error("Can get the value from LS");
+    }
+
+    // const valueToStore = typeof value !== "string" ? JSON.stringify(value) : value;
+
+    localStorage.getItem(key);
+}
+
+export const removeItemFromLocalStorage = (key) => {
+    if (!key) {
+        return console.error("Can not remove item from LS");
+    }
+
+    // const valueToStore = typeof value !== "string" ? JSON.stringify(value) : value;
+
+    localStorage.removeItem(key);
 }
 
 export const getFormBody = (params) => {
