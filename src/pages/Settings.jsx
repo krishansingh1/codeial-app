65aaa9d42ea2ab20a0ac132e35493dca649582cb
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 22:59:56 +0530

    Commit

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/settings.module.css';
+
+const Settings = () => {
+  return (
+    <div className={style.settings}>
+      <div className={styles.imgContainer}>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
