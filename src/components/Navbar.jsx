2b78f93dd0bc2d6ed087ebab3a507ec53b924cce
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 16:12:40 +0530

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,28 +16,38 @@ const Navbar = () => {
       </div>
 
       <div className={styles.rightNav}>
-        {auth.user && <div className={styles.user}>
-          <a href="/">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-              alt=""
-              className={styles.userDp}
-            />
-          </a>
-          <span>{auth.user.anme}</span>
-        </div>}
+        {auth.user && (
+          <div className={styles.user}>
+            <a href="/">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                alt=""
+                className={styles.userDp}
+              />
+            </a>
+            <span>{auth.user.name}</span>
+          </div>
+        )}
 
         <div className={styles.navLinks}>
           <ul>
-            <li>
-              <Link to={'/login'}>Login</Link>
-            </li>
-            <li>
-              <a href="/">Log out</a>
-            </li>
-            <li>
-              <a href="/">Register</a>
-            </li>
+            {auth.user ? (
+              <>
+                <li>
+                  <button onClick={auth.logout}>Log out</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to={'/login'}>Login</Link>
+                </li>
+
+                <li>
+                  <a href="/">Register</a>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
