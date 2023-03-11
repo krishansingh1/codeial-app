4dbde84de6e6c1a7e8fcbccc259b8f853846ddf8
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-11 16:54:11 +0530

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
                 </li>
 
                 <li>
-                  <a href="/">Register</a>
+                  <Link to={"/signup"}>Register</a>
                 </li>
               </>
             )}
