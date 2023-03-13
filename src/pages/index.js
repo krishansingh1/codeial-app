ccb112212122856fff390ae85c74aac9a4204d94
Krishan Kumar <kkrishan7074@gmail.com>
2023-03-13 10:47:29 +0530

    Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import { Home } from './Home';
-import { Login } from './Login'
+import { Login } from './Login';
+import { SignUp } from './SignUp';
+import { Settings } from './Settings';
+
 export {
-    Home, Login
+    Home, Login, SignUp, Settings
 }
